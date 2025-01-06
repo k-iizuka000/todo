d25aa3bbc8832ae390f7c1fa458b8bdbee510593
青木景志 <aoki@aokiki-mac.local>
2025-01-06 09:52:53 +0900

    雛形作成

---
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -0,0 +1,23 @@
+package com.example.todo.service;
+
+import com.example.todo.entity.Todo;
+import com.example.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo saveTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+} 
